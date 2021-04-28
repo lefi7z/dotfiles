a8d38ebc31f86cfbe0f34d7c616c9492e8bf3ef5
lefi7z <moritz.k-man@arcor.de>
2021-04-28 13:52:42 +0200

    Add tmux configuration file.

--- make_links.sh
+++ make_links.sh
@@ -3,7 +3,9 @@ set -xv
 
 CUR_DIR=$(pwd)
 
-ln -s ${CUR_DIR}/tmuxp ~/.tmuxp
 ln -s ${CUR_DIR}/zshrc ~/.zshrc
 ln -s ${CUR_DIR}/zshrc.local ~/.zshrc.local
+
+ln -s ${CUR_DIR}/tmuxp ~/.tmuxp
+ln -s ${CUR_DIR}/tmux.conf ~/.tmux.conf
 
